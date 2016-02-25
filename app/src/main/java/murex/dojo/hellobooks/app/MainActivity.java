e4ab676025d82f379bc2d39ecba47956f939adfe
Maher Malaeb <m.n.malaeb@gmail.com>
2016-02-25 23:38:28 +0200

    Create and display a list of dummy books on the main activity:  1-Create the Book class that will hold any book info  2-In the main activity:   2.1-Create a list of books and fill it with dummy data   2.2-Create a recycler view and attach an adapter and layout manager to it  3-In the BooksAdapter class:   3.1-This class handles providing data to the recycler view   3.2-It also handles mapping between each view of the item view and its corresponding data  4.Create books_list_item which will be the view of 1 element of the list  5. Add the recycler view to the main activity and remove the default hello world text view

--- app/src/main/java/murex/dojo/hellobooks/app/MainActivity.java
+++ app/src/main/java/murex/dojo/hellobooks/app/MainActivity.java
@@ -2,14 +2,42 @@ package murex.dojo.hellobooks.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+   private RecyclerView mRecyclerView;
+   private RecyclerView.Adapter mAdapter;
+   private RecyclerView.LayoutManager mLayoutManager;
+   private ArrayList<Book> booksList = new ArrayList<Book>();
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+      fillBooksListWithDummyData();
+
+      mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+      // use a linear layout manager
+      mLayoutManager = new LinearLayoutManager(this);
+      mRecyclerView.setLayoutManager(mLayoutManager);
+
+      // specify an adapter (see also next example)
+      mAdapter = new BooksAdapter(booksList);
+      mRecyclerView.setAdapter(mAdapter);
+
+   }
+
+   public void fillBooksListWithDummyData(){
+      Book newBook;
+      for(int i=0; i<100;i++){
+         newBook = new Book ("book");
+         booksList.add(newBook);
+      }
    }
 }
